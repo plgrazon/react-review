c44e9c1528ab350c153c30d8b68e24649050c3a6
Paolo Razon <plgrazon@gmail.com>
2018-05-12 17:12:42 -0700

    Correct file nesting.

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import react, { component } from 'react';
+inport TodoList from './TodoList.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state{
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      Hello from App Component
+      </div>
+    )
+  }
+}
+
+export default App;
